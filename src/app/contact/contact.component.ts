e7c4f8f7f418ccfa75255a0f8fb984065facfa22
p4cm4n972 <manuel.adele@gmail.com>
2018-04-10 16:56:25 +0200

    contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,13 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  optionsSelect: Array<any>;
   public map: any = {
     lat: 48.621483, lng: 2.330711, zoom: 10
   };
   constructor() { }
 
   ngOnInit() {
+    this.optionsSelect = [
+      { value: '1', label: 'Commercial' },
+      { value: '2', label: 'ARM' },
+      { value: '3', label: 'Support' },
+      { value: '', label: 'Marketing' },
+    ];
   }
 
 }
